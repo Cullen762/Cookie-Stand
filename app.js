eed73c184c550ac5b8781d1115577598cd39e679
Timothy Cullen <timothycullen@Timothys-MacBook-Pro.local>
2017-10-14 16:13:43 -0700

    success!

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var pike = {
   avgCkieHr:6.3,
   ranCustHr:[],
   cookiesSoldHr:[],
-  totalCookies:0,
+  totalCookies:10,
   //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
   calcRanCustHr: function(){
     for(var i = 0 ; i < hoursOpen.length; i++){
@@ -30,20 +30,28 @@ var pike = {
 
   },
 
-calcCookiesSoldHr: function(){
+  calcCookiesSoldHr: function(){
     for(var j = 0; j < hoursOpen.length; j++){
       console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
       this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
     }
 
   },
+  //augmented assignement
+  //totalCookies
+  calcTotalCookies: function(){
+    for( var l = 0 ; l < hoursOpen.length; l++){
+      this.totalCookies += this.cookiesSoldHr[l];
+    }
+  },
   //the render function, this dynamically populates the html page.
   render: function(){
     var pike = document.getElementById('pike');
     var fnp = document.getElementById('fnp');
-
+    //var total = document.getElementsById('total');
     this.calcRanCustHr();
     this.calcCookiesSoldHr();
+    this.calcTotalCookies();
     //appends the name of the object in the size h3 to the linked html sheet
     var h3EL = document.createElement('h3');
     h3EL.textContent = this.name;
@@ -52,26 +60,31 @@ calcCookiesSoldHr: function(){
     for(var k = 0; k < hoursOpen.length; k++){
       var liEl = document.createElement('li');
       console.log(this.cookiesSoldHr[k]);
-      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + ' cookies ';// this.calcCookiesSldHr had improper syntax, changed to cookiesSoldHr
-      // console.log both methods to verify they were working, found bugs in syntax, deleted a few ")'s'" code is now operational
+      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + 'cookies ';
 
       pike.appendChild(liEl);
     }
+    //appends total element to bottom of page
+    var totalEl = document.createElement('li');
+    totalEl.textContent = ' total ' + this.totalCookies;
 
+    pike.appendChild(totalEl);
   }
 };
+
 pike.render();
 
+// alki
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-// seatac
-var airport = {
-  name: 'seatac airport',
-  minCustHour: 3,
-  maxCustHour:24,
-  avgCkieHr:1.2,
+
+var alki = {
+  name: 'alki',
+  minCustHour: 23,
+  maxCustHour:65,
+  avgCkieHr:6.3,
   ranCustHr:[],
   cookiesSoldHr:[],
-  totalCookies:0,
+  totalCookies:10,
   //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
   calcRanCustHr: function(){
     for(var i = 0 ; i < hoursOpen.length; i++){
@@ -83,182 +96,232 @@ var airport = {
 
   calcCookiesSoldHr: function(){
     for(var j = 0; j < hoursOpen.length; j++){
-      //console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+      console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
       this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
     }
 
+  },
+  //augmented assignement
+  //totalCookies
+  calcTotalCookies: function(){
+    for( var l = 0 ; l < hoursOpen.length; l++){
+      this.totalCookies += this.cookiesSoldHr[l];
+    }
+  },
+  //the render function, this dynamically populates the html page.
+  render: function(){
+    var alkiShop = document.getElementById('alki');
+    var alk = document.getElementById('alk');
+    //var total = document.getElementsById('total');
+    this.calcRanCustHr();
+    this.calcCookiesSoldHr();
+    this.calcTotalCookies();
+    //appends the name of the object in the size h3 to the linked html sheet
+    var h3EL = document.createElement('h3');
+    h3EL.textContent = this.name;
+    alkiShop.appendChild(h3EL);
+    //appends the list, populated with random numbers created by the calcCookiesSldHr and calcranCustHr methods
+    for(var k = 0; k < hoursOpen.length; k++){
+      var liEl = document.createElement('li');
+      //console.log(this.cookiesSoldHr[k]);
+      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + 'cookies ';
+
+      alkiShop.appendChild(liEl);
+    }
+    //appends total element to bottom of page
+    var totalEl = document.createElement('li');
+    totalEl.textContent = ' total ' + this.totalCookies;
+
+    alkiShop.appendChild(totalEl);
+  }
+};
+
+alki.render();
+
+//Seattle Center
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+// first and Pike
+var seattleCenter = {
+  name: 'Seattle Center',
+  minCustHour: 23,
+  maxCustHour:65,
+  avgCkieHr:6.3,
+  ranCustHr:[],
+  cookiesSoldHr:[],
+  totalCookies:10,
+  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
+  calcRanCustHr: function(){
+    for(var i = 0 ; i < hoursOpen.length; i++){
+      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
+      //console.log(this.ranCustHr[i]);
+    }
+
+  },
+
+  calcCookiesSoldHr: function(){
+    for(var j = 0; j < hoursOpen.length; j++){
+      console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+    }
+
+  },
+  //augmented assignement
+  //totalCookies
+  calcTotalCookies: function(){
+    for( var l = 0 ; l < hoursOpen.length; l++){
+      this.totalCookies += this.cookiesSoldHr[l];
+    }
+  },
+  //the render function, this dynamically populates the html page.
+  render: function(){
+    var spaceNeedle = document.getElementById('seaCenter');
+    var seaCenter = document.getElementById('sec');
+    //var total = document.getElementsById('total');
+    this.calcRanCustHr();
+    this.calcCookiesSoldHr();
+    this.calcTotalCookies();
+    //appends the name of the object in the size h3 to the linked html sheet
+    var h3EL = document.createElement('h3');
+    h3EL.textContent = this.name;
+    spaceNeedle.appendChild(h3EL);
+    //appends the list, populated with random numbers created by the calcCookiesSldHr and calcranCustHr methods
+    for(var k = 0; k < hoursOpen.length; k++){
+      var liEl = document.createElement('li');
+      //console.log(this.cookiesSoldHr[k]);
+      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + 'cookies ';
+
+      spaceNeedle.appendChild(liEl);
+    }
+    //appends total element to bottom of page
+    var totalEl = document.createElement('li');
+    totalEl.textContent = ' total ' + this.totalCookies;
+
+    spaceNeedle.appendChild(totalEl);
+  }
+};
+
+seattleCenter.render();
+
+//capitol hill
+var capitolHill = {
+  name: 'Capitol Hill',
+  minCustHour: 23,
+  maxCustHour:65,
+  avgCkieHr:6.3,
+  ranCustHr:[],
+  cookiesSoldHr:[],
+  totalCookies:10,
+  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
+  calcRanCustHr: function(){
+    for(var i = 0 ; i < hoursOpen.length; i++){
+      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
+      //console.log(this.ranCustHr[i]);
+    }
+
+  },
+
+  calcCookiesSoldHr: function(){
+    for(var j = 0; j < hoursOpen.length; j++){
+      console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+    }
+
+  },
+  //augmented assignement
+  //totalCookies
+  calcTotalCookies: function(){
+    for( var l = 0 ; l < hoursOpen.length; l++){
+      this.totalCookies += this.cookiesSoldHr[l];
+    }
+  },
+  //the render function, this dynamically populates the html page.
+  render: function(){
+    var capHill = document.getElementById('cph');
+    var ch = document.getElementById('ch');
+    //var total = document.getElementsById('total');
+    this.calcRanCustHr();
+    this.calcCookiesSoldHr();
+    this.calcTotalCookies();
+    //appends the name of the object in the size h3 to the linked html sheet
+    var h3EL = document.createElement('h3');
+    h3EL.textContent = this.name;
+    capHill.appendChild(h3EL);
+    //appends the list, populated with random numbers created by the calcCookiesSldHr and calcranCustHr methods
+    for(var k = 0; k < hoursOpen.length; k++){
+      var liEl = document.createElement('li');
+      //console.log(this.cookiesSoldHr[k]);
+      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + 'cookies ';
+
+      capHill.appendChild(liEl);
+    }
+    //appends total element to bottom of page
+    var totalEl = document.createElement('li');
+    totalEl.textContent = ' total ' + this.totalCookies;
+
+    capHill.appendChild(totalEl);
+  }
+};
+
+capitolHill.render();
+
+//SeaTac Airport
+var seaTacAirport = {
+  name: 'SeaTac Airport',
+  minCustHour: 23,
+  maxCustHour:65,
+  avgCkieHr:6.3,
+  ranCustHr:[],
+  cookiesSoldHr:[],
+  totalCookies:10,
+  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
+  calcRanCustHr: function(){
+    for(var i = 0 ; i < hoursOpen.length; i++){
+      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
+      //console.log(this.ranCustHr[i]);
+    }
+
+  },
+
+  calcCookiesSoldHr: function(){
+    for(var j = 0; j < hoursOpen.length; j++){
+      console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
+    }
+
+  },
+  //augmented assignement
+  //totalCookies
+  calcTotalCookies: function(){
+    for( var l = 0 ; l < hoursOpen.length; l++){
+      this.totalCookies += this.cookiesSoldHr[l];
+    }
   },
   //the render function, this dynamically populates the html page.
   render: function(){
     var seaTac = document.getElementById('airport');
     var sta = document.getElementById('sta');
-
+    //var total = document.getElementsById('total');
     this.calcRanCustHr();
     this.calcCookiesSoldHr();
+    this.calcTotalCookies();
     //appends the name of the object in the size h3 to the linked html sheet
     var h3EL = document.createElement('h3');
     h3EL.textContent = this.name;
     seaTac.appendChild(h3EL);
-
+    //appends the list, populated with random numbers created by the calcCookiesSldHr and calcranCustHr methods
     for(var k = 0; k < hoursOpen.length; k++){
       var liEl = document.createElement('li');
-      console.log(seaTac);
+      //console.log(this.cookiesSoldHr[k]);
+      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + 'cookies ';
+
       seaTac.appendChild(liEl);
-      //console.log(this.cookiesSoldHr[k]);
-      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + ' cookies ';
-      //naming conflicts
     }
+    //appends total element to bottom of page
+    var totalEl = document.createElement('li');
+    totalEl.textContent = ' total ' + this.totalCookies;
 
+    seaTac.appendChild(totalEl);
   }
 };
-airport.render();
 
-var alki = {
-  name: 'alki',
-  minCustHour: 3,
-  maxCustHour:24,
-  avgCkieHr:1.2,
-  ranCustHr:[],
-  cookiesSoldHr:[],
-  totalCookies:0,
-  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
-  calcRanCustHr: function(){
-    for(var i = 0 ; i < hoursOpen.length; i++){
-      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
-      //console.log(this.ranCustHr[i]);
-    }
-
-  },
-
-  calcCookiesSoldHr: function(){
-    for(var j = 0; j < hoursOpen.length; j++){
-      //console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-    }
-
-  },
-  //the render function, this dynamically populates the html page.
-  //***
-  render: function(){
-    var alkiShop = document.getElementById('alki');
-    var alk = document.getElementById('alk');
-    //naming conflict
-    this.calcRanCustHr();
-    this.calcCookiesSoldHr();
-    //appends the name of the object in the size h3 to the linked html sheet
-    var h3EL = document.createElement('h3');
-    h3EL.textContent = this.name;
-    alkiShop.appendChild(h3EL);
-
-    for(var k = 0; k < hoursOpen.length; k++){
-      var liEl = document.createElement('li');
-      //console.log(alkiShop);
-      alkiShop.appendChild(liEl);
-      //console.log(this.cookiesSoldHr[k]);
-      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + ' cookies ';
-      //naming conflicts
-    }
-
-  }
-};
-alki.render();
-
-var seaCenter = {
-  name: 'seattle center',
-  minCustHour: 3,
-  maxCustHour:24,
-  avgCkieHr:1.2,
-  ranCustHr:[],
-  cookiesSoldHr:[],
-  totalCookies:0,
-  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
-  calcRanCustHr: function(){
-    for(var i = 0 ; i < hoursOpen.length; i++){
-      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
-      //console.log(this.ranCustHr[i]);
-    }
-
-  },
-
-  calcCookiesSoldHr: function(){
-    for(var j = 0; j < hoursOpen.length; j++){
-      //console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-    }
-
-  },
-  //the render function, this dynamically populates the html page.
-  //***
-  render: function(){
-    var spaceNeedle = document.getElementById('seaCenter');
-    var sec = document.getElementById('sec');
-    //naming conflict
-    this.calcRanCustHr();
-    this.calcCookiesSoldHr();
-    //appends the name of the object in the size h3 to the linked html sheet
-    var h3EL = document.createElement('h3');
-    h3EL.textContent = this.name;
-    spaceNeedle.appendChild(h3EL);
-
-    for(var k = 0; k < hoursOpen.length; k++){
-      var liEl = document.createElement('li');
-      //console.log(alkiShop);
-      spaceNeedle.appendChild(liEl);
-      //console.log(this.cookiesSoldHr[k]);
-      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + ' cookies ';
-      //naming conflicts
-    }
-
-  }
-};
-seaCenter.render();
-
-var capHill = {
-  name: 'capitol hill',
-  minCustHour: 3,
-  maxCustHour:24,
-  avgCkieHr:1.2,
-  ranCustHr:[],
-  cookiesSoldHr:[],
-  totalCookies:0,
-  //The methods (properties that act like functions) are built with a for loop that cycles through the hoursOpen array. It uses Math.floor(Math.random) and this. to calculate the number of customers min/max and  .push to populate the array.  //method for random customers
-  calcRanCustHr: function(){
-    for(var i = 0 ; i < hoursOpen.length; i++){
-      this.ranCustHr.push(Math.floor(Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour));
-      //console.log(this.ranCustHr[i]);
-    }
-
-  },
-
-  calcCookiesSoldHr: function(){
-    for(var j = 0; j < hoursOpen.length; j++){
-      //console.log(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-      this.cookiesSoldHr.push(Math.round(this.avgCkieHr * this.ranCustHr[j]));
-    }
-
-  },
-  //the render function, this dynamically populates the html page.
-  //***
-  render: function(){
-    var capitolHill = document.getElementById('captlHill');
-    var ch = document.getElementById('ch');
-    //naming conflict
-    this.calcRanCustHr();
-    this.calcCookiesSoldHr();
-    //appends the name of the object in the size h3 to the linked html sheet
-    var h3EL = document.createElement('h3');
-    h3EL.textContent = this.name;
-    capitolHill.appendChild(h3EL);
-
-    for(var k = 0; k < hoursOpen.length; k++){
-      var liEl = document.createElement('li');
-      //console.log(alkiShop);
-      capitolHill.appendChild(liEl);
-      //console.log(this.cookiesSoldHr[k]);
-      liEl.textContent = hoursOpen[k] + ' : ' + this.cookiesSoldHr[k] + ' cookies ';
-      //naming conflicts
-    }
-
-  }
-};
-capHill.render();
+seaTacAirport.render();
